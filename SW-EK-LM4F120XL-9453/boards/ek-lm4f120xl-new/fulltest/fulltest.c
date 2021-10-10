788bece0b6100b9c61cfb886fb904fd4b117a68a
syabyr <mybays@sina.com>
2021-10-10 22:25:46 +0800

    cmake

---
+++ SW-EK-LM4F120XL-9453/boards/ek-lm4f120xl-new/fulltest/fulltest.c
@@ -0,0 +1,182 @@
+
+#include <stdio.h>
+
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+//*****************************************************************************
+//
+//! \addtogroup example_list
+//! <h1>UART Echo (uart_echo)</h1>
+//!
+//! This example application utilizes the UART to echo text.  The first UART
+//! (connected to the USB debug virtual serial port on the evaluation board)
+//! will be configured in 115,200 baud, 8-n-1 mode.  All characters received on
+//! the UART are transmitted back to the UART.
+//
+//*****************************************************************************
+
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, unsigned long ulLine)
+{
+}
+#endif
+
+//*****************************************************************************
+//
+// The UART interrupt handler.
+//
+//*****************************************************************************
+void
+UARTIntHandler(void)
+{
+    unsigned long ulStatus;
+
+    //
+    // Get the interrrupt status.
+    //
+    ulStatus = ROM_UARTIntStatus(UART0_BASE, true);
+
+    //
+    // Clear the asserted interrupts.
+    //
+    ROM_UARTIntClear(UART0_BASE, ulStatus);
+
+    //
+    // Loop while there are characters in the receive FIFO.
+    //
+    while(ROM_UARTCharsAvail(UART0_BASE))
+    {
+        //
+        // Read the next character from the UART and write it back to the UART.
+        //
+        ROM_UARTCharPutNonBlocking(UART0_BASE,
+                                   ROM_UARTCharGetNonBlocking(UART0_BASE));
+        
+        //
+        // Blink the LED to show a character transfer is occuring.
+        //
+        ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+        
+        //
+        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
+        //
+        ROM_SysCtlDelay(SysCtlClockGet() / (1000 * 3));
+        
+        //
+        // Turn off the LED
+        //
+        ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+        
+    }
+}
+
+
+
+void DelayMs(unsigned long ulCount)
+{
+    unsigned long freq=ROM_SysCtlClockGet();
+    unsigned long mscount=freq/3000;
+    ROM_SysCtlDelay(mscount*ulCount);
+}
+
+
+int main(void)
+{
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+    ROM_FPUEnable();
+    ROM_FPULazyStackingEnable();
+
+    //
+    // Set the clocking to run directly from the crystal.
+    //
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+
+    //
+    // Enable the GPIO port that is used for the on-board LED.
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    //
+    // Enable the GPIO pins for the LED (PF2).  
+    //
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+
+    //
+    // Enable the peripherals used by this example.
+    //
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    //
+    // Enable processor interrupts.
+    //
+    ROM_IntMasterEnable();
+
+
+    //
+    // Set GPIO A0 and A1 as UART pins.
+    //
+    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //
+    // Configure the UART for 115,200, 8-N-1 operation.
+    //
+    ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200,
+                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                             UART_CONFIG_PAR_NONE));
+
+    //
+    // Enable the UART interrupt.
+    //
+    ROM_IntEnable(INT_UART0);
+    ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+
+
+    printf("helloworld.\r\n");
+    unsigned long freq=ROM_SysCtlClockGet();
+    //printf("freq:%ldHz\r\n",freq);
+    //printf("freq:%sHz\r\n","hello");
+    float test=124575567687655654.84454383838383838383;
+    //
+    // Loop forever echoing data through the UART.
+    //
+    while(1)
+    {
+
+        printf("helloworld.\r\n");
+        //SysCtlDelay(2000000);
+        //printf("\r\n");
+        //printf("freq:%ldHz\r\n\r\n",freq);
+        printf("freq:%ldHz\r\n",freq);
+        //printf("float:%f\r\n",test);
+        //printf("\r\n");
+        //printf("helloworld.\r\n");
+        //printf("freq:%dHz\r\n\r\n",100);
+        //ROM_SysCtlDelay(80000000);
+        DelayMs(1000);
+    }
+}
