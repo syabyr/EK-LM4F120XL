29441c1f0199e119ce9ac261dfacf76d62b373b5
syabyr <62129949+syabyr@users.noreply.github.com>
2023-01-11 19:01:39 +0800

    Update main.c

--- platformio_proj/native/blink/src/main.c
+++ platformio_proj/native/blink/src/main.c
@@ -3,23 +3,25 @@
  * See LICENSE for details.
  */
 
-#include <stdint.h>
-#include <stdbool.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
 #include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
 
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
-
-void setup() {
-    SysCtlClockSet(
-            SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
-}
 
 void loop() {
     GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN, LED_RED);
@@ -29,3 +31,17 @@ void loop() {
     GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN, LED_GREEN);
     SysCtlDelay(3000000);
 }
+
+void main() {
+    SysCtlClockSet(
+            SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
+    while(1)
+    {
+        loop();
+    }
+}
+
+
