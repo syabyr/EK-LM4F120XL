83b5c26e5c6a9534ca498214738d0d9dafa9cac9
syabyr <mybays@sina.com>
2023-03-19 01:59:28 +0800

    blinky vscode openocd

---
+++ SW-EK-LM4F120XL-9453/boards/ek-lm4f120xl-new/blinky/blinky.c
@@ -0,0 +1,48 @@
+#include "inc/lm4f120h5qr.h"
+#include <stdio.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+
+
+int main(void)
+{
+    volatile unsigned long ulLoop;
+
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    ulLoop = SYSCTL_RCGC2_R;
+
+    ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3);
+    //
+    // Loop forever.
+    //
+    while(1)
+    {
+        //
+        // Turn on the LED.
+        ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3, GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3);
+
+
+        //
+        // Delay for a bit.
+        //
+        for(ulLoop = 0; ulLoop < 200000; ulLoop++)
+        {
+        }
+
+        //
+        // Turn off the LED.
+        //
+        ROM_GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 |GPIO_PIN_2 |GPIO_PIN_3, 0x0);
+        // Delay for a bit.
+        //
+        for(ulLoop = 0; ulLoop < 200000; ulLoop++)
+        {
+        }
+    }
+}
