2d9f83db8fb637ab705ed7968fdf6f508129cd6e
syabyr <mybays@sina.com>
2023-03-27 22:33:05 +0800

    printf

--- SW-EK-LM4F120XL-9453/boards/ek-lm4f120xl-new/drivers/retargetserial.c
+++ SW-EK-LM4F120XL-9453/boards/ek-lm4f120xl-new/drivers/retargetserial.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <sys/stat.h>
-#include "inc/hw_memmap.h"
+#include <sys/types.h>
+
 #include "driverlib/rom.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/uart.h"
 
 #ifdef __GNUC__
 int _write(int fd, char *ptr, int len)
 {
-    int count=len;
-    while(count--)
+    int ulCount = len;
+    char *pucBuffer = ptr;
+    while(ulCount--)
     {
-        ROM_UARTCharPut(UART0_BASE, *ptr++);
+        ROM_UARTCharPut(UART0_BASE, *pucBuffer++);
     }
     return len;
 }
@@ -17,14 +23,24 @@ int _write(int fd, char *ptr, int len)
 
 caddr_t _sbrk(int incr)
 {
-    return (caddr_t) 0;
+    return (caddr_t) 0x20007000;
 }
-
+int _close(int file)
+{
+    return -1;
+}
 int _fstat(int file, struct stat *st)
 {
     return 0;
 }
-
+int _isatty(int file)
+{
+    return 1;
+}
+int _lseek(int file, int ptr, int dir)
+{
+    return 0;
+}
 int _read(int file, char *ptr, int len)
 {
     return 0;
--- SW-EK-LM4F120XL-9453/boards/ek-lm4f120xl-new/printf/printf.c
+++ SW-EK-LM4F120XL-9453/boards/ek-lm4f120xl-new/printf/printf.c
@@ -1,27 +1,3 @@
-//*****************************************************************************
-//
-// uart_echo.c - Example for reading data from and writing data to the UART in
-//               an interrupt driven fashion.
-//
-// Copyright (c) 2012 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-// 
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-// 
-// This is part of revision 9453 of the EK-LM4F120XL Firmware Package.
-//
-//*****************************************************************************
 
 #include <stdio.h>
 #include <sys/stat.h>
@@ -37,25 +13,9 @@
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
-
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>UART Echo (uart_echo)</h1>
-//!
-//! This example application utilizes the UART to echo text.  The first UART
-//! (connected to the USB debug virtual serial port on the evaluation board)
-//! will be configured in 115,200 baud, 8-n-1 mode.  All characters received on
-//! the UART are transmitted back to the UART.
-//
-//*****************************************************************************
+#include <string.h>
 
 
-//*****************************************************************************
-//
-// The error routine that is called if the driver library encounters an error.
-//
-//*****************************************************************************
 #ifdef DEBUG
 void
 __error__(char *pcFilename, unsigned long ulLine)
@@ -68,8 +28,7 @@ __error__(char *pcFilename, unsigned long ulLine)
 // The UART interrupt handler.
 //
 //*****************************************************************************
-void
-UARTIntHandler(void)
+void UARTIntHandler(void)
 {
     unsigned long ulStatus;
 
@@ -134,40 +93,7 @@ UARTSend(const unsigned char *pucBuffer, unsigned long ulCount)
 }
 
 
-#ifdef __GNUC__
-int _write(int fd, char *ptr, int len)
-{
-  UARTSend((unsigned char *)ptr, len);
-  return len;
-}
-#endif
 
-caddr_t _sbrk(int incr) 
-{
-    return (caddr_t) 0;
-}
-/*
-int _close(int file) 
-{ 
-    return -1; 
-}
-int _fstat(int file, struct stat *st) 
-{ 
-    return 0;
-}
-int _isatty(int file) 
-{ 
-    return 1; 
-}
-int _lseek(int file, int ptr, int dir) 
-{ 
-    return 0; 
-}
-int _read(int file, char *ptr, int len) 
-{ 
-    return 0;
-}
-*/
 //*****************************************************************************
 //
 // This example demonstrates how to send a string of data to the UART.
@@ -237,11 +163,28 @@ main(void)
     UARTSend((unsigned char *)"\033[2JEnter text: \r\n", 18);
 
 
-    printf("helloworld.\r\n");
+    printf("helloworld123.\r\n");
     unsigned long freq=ROM_SysCtlClockGet();
+    printf("Changing system clock to %ldMHz\n\r",
+			       freq);
+    //float test=124575567687655654.84454383838383838383;
+   // 
+    while(1)
+    {
+        printf("helloworld.\r\n");
+        //printf("\r\n");
+        //printf("freq:%ldHz\r\n\r\n",freq);
+        //printf("freq:%dHz\r\n",freq);
+        //printf("float:%f\r\n",test);
+        //printf("\r\n");
+        //printf("helloworld.\r\n");
+        //printf("freq:%dHz\r\n\r\n",100);
+        ROM_SysCtlDelay(10000000);
+    }
+    //unsigned long freq=ROM_SysCtlClockGet();
     //printf("freq:%ldHz\r\n",freq);
     //printf("freq:%sHz\r\n","hello");
-    float test=124575567687655654.84454383838383838383;
+    
     //
     // Loop forever echoing data through the UART.
     //
@@ -252,8 +195,8 @@ main(void)
         //SysCtlDelay(2000000);
         //printf("\r\n");
         //printf("freq:%ldHz\r\n\r\n",freq);
-        printf("freq:%ldHz\r\n",freq);
-        printf("float:%f\r\n",test);
+        //printf("freq:%ldHz\r\n",freq);
+        //printf("float:%f\r\n",test);
         //printf("\r\n");
         //printf("helloworld.\r\n");
         //printf("freq:%dHz\r\n\r\n",100);
